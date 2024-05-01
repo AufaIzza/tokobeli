31c09e1e33a4345f5ef2dbee476a3484b9cd1065
AufaIzza <aufashiqiizza@gmail.com>
2024-05-01 12:38:29 +0700

    Creating Routers and Index page

---
+++ src/routers/routers.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Index from "../pages/index/index"
+
+const routers = () => {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Index/>
+        }
+    ])
+
+    return (
+        <RouterProvider router={router}/>
+    )
+}
+
+export default routers
