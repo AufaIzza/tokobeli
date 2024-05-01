41c3830972b9c05bb91028fc7df5c3ca5929137a
AufaIzza <aufashiqiizza@gmail.com>
2024-05-01 13:15:04 +0700

    Testing zustand persist state

--- src/store/storeList.js
+++ src/store/storeList.js
@@ -1,7 +1,13 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useStoreList = create( (set) => ({
+export const useStoreList = create(persist( (set) => ({
     amount: 40,
     increaseAmountByOne: () => set((state) => ({ amount: state.amount + 1}))
-})
+}),
+ {
+    name: "amountStorage",
+    storage: createJSONStorage(() => sessionStorage)
+ }
+) 
 )
